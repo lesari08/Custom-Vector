3c8c8e0c08941b2337dfb78f09843996e1b35be1
lesari08 <lesari08@gmail.com>
2023-07-15 19:10:33 -0500

    no-op change

--- tests/UnitTests_CustomVector.cpp
+++ tests/UnitTests_CustomVector.cpp
@@ -28,7 +28,6 @@ TEST(MemoryManger, constructor)
     EXPECT_EQ(mgr.block_start + size, mgr.block_end);
 }
 
-
 TEST(MemoryManger, moveConstructor)
 {
     using MemoryManger = Vector_Memory_Manager<int, std::allocator<int>>;
